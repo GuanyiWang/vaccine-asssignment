c6d7f872e06bd4ea0d8aa85aaf8c25f4ff126a08
Guanyi Wang <51835959+GuanyiWang@users.noreply.github.com>
2021-07-13 18:21:47 +0800

    Delete Network_Generation directory

--- Network_Generation/selfloops.m
+++
@@ -1,8 +0,0 @@
-% counts the number of self-loops in the graph
-% INPUT: adjacency matrix
-% OUTPUT: interger, number of self-loops
-% cited from http://strategic.mit.edu/downloads.php?page=matlab_networks (Matlab Tools for Network Analysis ) 
-
-function sl=selfloops(adj)
-
-sl=sum(diag(adj));
